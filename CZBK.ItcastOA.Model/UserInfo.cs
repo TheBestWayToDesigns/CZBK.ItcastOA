73c2e4c51e299fa7caad24066bcdf989d27c12ef
Administrator <Administrator@MASTER>
2017-12-14 08:13:11 +0800

    zty

--- CZBK.ItcastOA.Model/UserInfo.cs
+++ CZBK.ItcastOA.Model/UserInfo.cs
@@ -37,10 +37,6 @@ namespace CZBK.ItcastOA.Model
             this.T_WinBakFaHuo1 = new HashSet<T_WinBakFaHuo>();
             this.Schedules = new HashSet<Schedule>();
             this.ScheduleUsers = new HashSet<ScheduleUser>();
-            this.T_BaoXiaoBill = new HashSet<T_BaoXiaoBill>();
-            this.T_JieKuanBill = new HashSet<T_JieKuanBill>();
-            this.GongLuBaoXiaoBills = new HashSet<GongLuBaoXiaoBill>();
-            this.Schedules1 = new HashSet<Schedule>();
         }
     
         public int ID { get; set; }
@@ -105,13 +101,5 @@ namespace CZBK.ItcastOA.Model
         public virtual ICollection<Schedule> Schedules { get; set; }
         [JsonIgnore]
         public virtual ICollection<ScheduleUser> ScheduleUsers { get; set; }
-        [JsonIgnore]
-        public virtual ICollection<T_BaoXiaoBill> T_BaoXiaoBill { get; set; }
-        [JsonIgnore]
-        public virtual ICollection<T_JieKuanBill> T_JieKuanBill { get; set; }
-        [JsonIgnore]
-        public virtual ICollection<GongLuBaoXiaoBill> GongLuBaoXiaoBills { get; set; }
-        [JsonIgnore]
-        public virtual ICollection<Schedule> Schedules1 { get; set; }
     }
 }
