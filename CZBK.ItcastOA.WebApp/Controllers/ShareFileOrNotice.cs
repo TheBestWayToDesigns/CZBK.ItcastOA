d52d516cdad7fd9b1db7e26c3774b2ef2ced00a0
les_lau <les_lau@163.com>
2017-12-18 21:47:08 +0800

    liusong12-18-21:46

--- CZBK.ItcastOA.WebApp/Controllers/ShareFileOrNotice.cs
+++ CZBK.ItcastOA.WebApp/Controllers/ShareFileOrNotice.cs
@@ -15,6 +15,8 @@ namespace CZBK.ItcastOA.WebApp.Controllers
         IBLL.IShareFileOrNoticeService ShareFileOrNoticeService;
         IBLL.IShareTypeService ShareTypeService;
         IBLL.IBumenInfoSetService BumenInfoSetService;
+        IBLL.IUserInfoService UserInfoService;
+        IBLL.IFileTypeService FileTypeService;
 
         //
         // GET: /ShareFileOrNotice/
@@ -44,21 +46,61 @@ namespace CZBK.ItcastOA.WebApp.Controllers
         //获取所有部门名称
         public ActionResult GetAllBuMen()
         {
-            var temp = BumenInfoSetService.LoadEntities(x => x.ID > 0).DefaultIfEmpty();
-            return Json(temp,JsonRequestBehavior.AllowGet);
+            var temp = BumenInfoSetService.LoadEntities(x => x.ID > 0).DefaultIfEmpty().ToList();
+            List<STUBuMen> list = new List<STUBuMen>();
+            foreach(var a in temp)
+            {
+                STUBuMen stubm = new STUBuMen();
+                stubm.ID = a.ID;
+                stubm.Name = a.Name;
+                list.Add(stubm);
+            }
+            return Json(list,JsonRequestBehavior.AllowGet);
         }
         //获取部门用户名字
         public ActionResult GetShareToUser()
         {
-            var temp = BumenInfoSetService.LoadEntities(x => x.ID > 0).DefaultIfEmpty();
-            return Json(temp, JsonRequestBehavior.AllowGet);
+            var bmid = Convert.ToInt32(Request["BMID"]);
+            var temp = UserInfoService.LoadEntities(x => x.BuMenID == bmid).DefaultIfEmpty().ToList();
+            List<BMUser> list = new List<BMUser>();
+            foreach (var a in temp)
+            {
+                BMUser bmu = new BMUser();
+                bmu.ID = a.ID;
+                bmu.Name = a.PerSonName;
+                list.Add(bmu);
+            }
+            return Json(list, JsonRequestBehavior.AllowGet);
         }
-
+        //获取所有文件类型
+        public ActionResult GetAllFileType()
+        {
+            var temp = FileTypeService.LoadEntities(x => x.ID > 0).DefaultIfEmpty().ToList();
+            List<FileTP> list = new List<FileTP>();
+            foreach (var a in temp)
+            {
+                FileTP ftp = new FileTP();
+                ftp.ID = a.ID;
+                ftp.Name = a.FileTypeCHNName;
+                list.Add(ftp);
+            }
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
 
         public class STUBuMen
         {
-            private int ID { get; set; }
-            private string Name { get; set; }
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+        public class BMUser
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+        }
+        public class FileTP
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
         }
     }
 }
